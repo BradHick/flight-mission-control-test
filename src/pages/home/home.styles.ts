3f58feda4a423737600a3ca26cbc11aa871bd8ae
BradHick <bradleyhrm@gmail.com>
2024-07-10 19:42:32 -0300

    Configure project

---
+++ src/pages/home/home.styles.ts
@@ -0,0 +1,9 @@
+import { tv } from 'tailwind-variants'
+
+export const page = tv({
+  base: 'w-[1119px] h-[769px]'
+})
+
+export const container = tv({
+  base: 'w-[100%] h-[686px] flex p-6'
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unfonts from 'unplugin-fonts/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    Unfonts({
+      google: {
+        families: [
+          {
+            name: 'Open Sans',
+            styles: 'ital,wght@0,300..800;1,300..800'
+          },
+          {
+            name: 'Inter',
+            styles: 'wght@100..900'
+          },
+          {
+            name: 'Lato',
+            styles:
+              'ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900'
+          }
+        ]
+      }
+    }),
+    react()
+  ],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
